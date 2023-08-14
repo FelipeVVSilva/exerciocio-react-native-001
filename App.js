9bad2c99852e920e772bbcf4fa469f3373e3f8de
FelipeVeigaVellosoDaSilva <felipe.veiga.trabalho@gmail.com>
2023-08-14 09:34:06 -0300

    Projeto inicial

--- App.js
+++ App.js
@@ -1,20 +1,134 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [matricula, setMatricula] = useState('');
+  const [nome, setNome] = useState('');
+  const [logradouro, setLogradouro] = useState('');
+  const [numero, setNumero] = useState('');
+  const [bairro, setBairro] = useState('');
+  const [cep, setCep] = useState('');
+  const [cidade, setCidade] = useState('');
+  const [uf, setUf] = useState('');
+
+  const alunosEquipe = [
+    { nome: 'Felipe Veiga Velloso da Silva', matricula: '202104478763' },
+    { nome: 'Bruno da Veiga Ramos', matricula: '202002457309' },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Empresa XPTO</Text>
+      <Image
+        source={require('./assets/LogoXPTO.jpg')}
+        style={styles.logo}
+      />
+      <Text style={styles.label}>Cadastramento de Clientes</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Matrícula"
+        value={matricula}
+        onChangeText={setMatricula}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Nome"
+        value={nome}
+        onChangeText={setNome}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Logradouro"
+        value={logradouro}
+        onChangeText={setLogradouro}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Número"
+        value={numero}
+        onChangeText={setNumero}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Bairro"
+        value={bairro}
+        onChangeText={setBairro}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="CEP"
+        value={cep}
+        onChangeText={setCep}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Cidade"
+        value={cidade}
+        onChangeText={setCidade}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="UF"
+        value={uf}
+        onChangeText={setUf}
+      />
+      <ScrollView style={styles.scrollView}>
+        {alunosEquipe.map((aluno, index) => (
+          <Text key={index} style={styles.aluno}>
+            Nome: {aluno.nome}, Matrícula: {aluno.matricula}
+          </Text>
+        ))}
+      </ScrollView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 20,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  input: {
+    width: '100%',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 10,
+  },
+  scrollView: {
+    marginTop: 20,
+    width: '100%',
+    maxHeight: 200, // Ajuste este valor conforme necessário
+    borderWidth: 1,
+    borderColor: 'gray',
+  },
+  aluno: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
 });
+
+export default App;
